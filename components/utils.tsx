8a745413fa31d3b8d4302f1bfd874a07492ec4a5
Dev43 <patrick@bitbackapp.com>
2023-02-06 18:13:11 +0200

    update url check

--- components/utils.tsx
+++ components/utils.tsx
@@ -2,21 +2,19 @@ import { useEffect, useState } from "react";
 
 export const isValidUrl = (url: string) => {
   try {
-    let u = url.replace("www.", "");
-    u = u.replace(/^(https?:\/\/)?/, "https://");
-    // if it isn't, throws an error
-    new URL(u);
-
-    // then we regexp
-    var urlPattern = new RegExp(
-      "^(https?://)?(([da-z.-]+).)?([da-z.-]+).([a-z.]{2,6})([/w .-]*)*/?$"
-    );
-    return urlPattern.test(u);
+    return isURL(url);
   } catch (e) {
     console.error(e);
     return false;
   }
 };
+
+function isURL(str: string) {
+  var urlRegex =
+    "^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$";
+  var url = new RegExp(urlRegex, "i");
+  return str.length < 2083 && url.test(str);
+}
 
 export const Toast = ({
   message,
