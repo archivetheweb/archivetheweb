15d2d7f0cacaaadd278e3a16cc6f57d586929eeb
Dev43 <patrick@bitbackapp.com>
2023-03-07 09:19:14 +0200

    remove walletconnector

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { mainnet, configureChains, createClient, WagmiConfig } from "wagmi";
 import ConnectorContext, { emptyState } from "../context/connector";
-
-const chains = [mainnet];
-
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: process.env.WEB3_MODAL_PROJECT_ID || "" }),
-]);
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: modalConnectors({ appName: "web3Modal", chains }),
-  provider,
-});
-
-const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -32,16 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <WagmiConfig client={wagmiClient}>
-        <ConnectorContext.Provider value={emptyState}>
-          <Component {...pageProps} />
-        </ConnectorContext.Provider>
-      </WagmiConfig>
-
-      <Web3Modal
-        projectId={process.env.WEB3_MODAL_PROJECT_ID}
-        ethereumClient={ethereumClient}
-      />
+      <ConnectorContext.Provider value={emptyState}>
+        <Component {...pageProps} />
+      </ConnectorContext.Provider>
     </>
   );
 }
