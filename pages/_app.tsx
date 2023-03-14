546d5d126e54622d41f86ff866f25981a65d474f
Dev43 <patrick@bitbackapp.com>
2023-03-14 09:49:02 +0200

    adds metadata to webpage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,21 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Archive the Web</title>
-        <meta name="description" content="Archive the web frontend" />
+        <meta
+          name="description"
+          content="Archive the Web is an open-source website archiving tool that allows you to set up automated archiving stored on Arweave. Our mission at Archive the Web is to create a decentralized backup of the world wide web together."
+        />
+        <meta
+          property="og:description"
+          content="Archive the Web is an open-source website archiving tool that allows you to set up automated archiving stored on Arweave. Our mission at Archive the Web is to create a decentralized backup of the world wide web together."
+        />
+        <meta property="og:title" content="Archive The Web" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://arweave.net/HSXsrUHoSsMP7hqkTJe_NhEzD5W77rg4OsLhy1xYTqs"
+        />
+        <meta property="og:url" content="https://archivetheweb.com/" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
